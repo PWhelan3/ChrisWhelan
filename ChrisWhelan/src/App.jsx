74a1291a138fff5ed09100a91b21fb82a5b2f8e9
Peter <pwhelanstudio@gmail.com>
2024-02-29 14:51:14 +0000

    Adding work component

--- ChrisWhelan/src/App.jsx
+++ ChrisWhelan/src/App.jsx
@@ -1,10 +1,8 @@
 import styles from "./App.module.css";
-import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
-import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Projects } from "./components/Projects/Projects";
+import { Work } from "./components/Work/Work";
 
 function App() {
   return (
