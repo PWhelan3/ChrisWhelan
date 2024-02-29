4bcf71a07361924f06020d35b3de270d580bf6ac
Peter <pwhelanstudio@gmail.com>
2024-02-29 15:06:12 +0000

    Edits to Nav and Hero

--- ChrisWhelan/src/Components/Hero/Hero.jsx
+++ ChrisWhelan/src/Components/Hero/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={styles.container}>
+        <img src={getImageUrl("123.JPG")} alt="Hero Of Me" className={styles.heroImg}/>
+    </section>
   )
 }
--- ChrisWhelan/src/Components/Navbar/Navbar.jsx
+++ ChrisWhelan/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="/">Peter.W</a>
+        <a className={styles.title} href="/">Chris Whelan</a>
         <div className={styles.menu}>
             <img className={styles.menuBtn}
                     src={
@@ -23,13 +23,7 @@ export const Navbar = () => {
                 onClick={() => setMenuOpen(false)}
             >
                 <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
+                    <a href="#projects">Work</a>
                 </li>
                 <li>
                     <a href="#contact">Contact</a>
